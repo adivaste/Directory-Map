cfed5fba138257dc2d8fb2e63db59178c0fedd5f
Aditya Vaste <adi45v@gmail.com>
2022-04-20 17:23:47 +0530

    Added the Feature to store the map in the file : Directory tree will be stored in 'mapFile.txt' file, so you access it anytime

--- listDir.py
+++ listDir.py
@@ -5,8 +5,18 @@ import time
 path = os.getcwd()
 tabs = 0
 
-print("________________________________________________________________________________________")
-print( "\n" + path) 
+# Creatign the File And Adding the map
+mapFile = open("mapFile.txt","w")
+mapFile.close()
+
+mapFile = open("mapFile.txt","a")
+
+print("________________________________________________________________________________________\n")
+mapFile.write("________________________________________________________________________________________\n")
+
+print( "\n" + path + "\n") 
+mapFile.write("\n" + path + "\n")
+
 def printDirs(path,tabs):
 	dirs = os.listdir(path)
 	
@@ -15,12 +25,16 @@ def printDirs(path,tabs):
 		# Identify the file
 		if (os.path.isfile(path+"/"+fileObj)):
 			print("\t"*tabs + "|_ " + fileObj)
+			mapFile.write("\t"*tabs + "|_ " + fileObj+"\n")
 		elif (os.path.isdir(path+ "/" + fileObj)):
 			print("\t"*tabs +  "|_ " + fileObj)
+			mapFile.write("\t"*tabs + "|_ " + fileObj+"\n")
 			printDirs(path+"/"+fileObj,tabs)
 	tabs = tabs -1
 printDirs(path,tabs)
-print("________________________________________________________________________________________")
+print("\n________________________________________________________________________________________")
+mapFile.write("\n________________________________________________________________________________________")
+mapFile.close()
 
 # Identifying the file and folder
 
@@ -28,19 +42,3 @@ print("_________________________________________________________________________
 #	1) Having a only one "." in whole filename and that is not at "0" index
 #	2) Having a two "." in filename, one at only "0"th index and another at any other index
 
-
-'''
-def printDirs(path,tabs):
-  9         dirs = os.listdir(path)
- 10
- 11         tabs = tabs + 1
- 12         for fileObj in dirs:
- 13                 # Identify the file
- 14                 if ( (fileObj.rfind(".") > 0 ) or (fileObj.find(".") == 0) and (fileObj.rfind(".") != 0) ):
- 15                         print("\t"*tabs + "--" + fileObj)
- 16                 else:
- 17                         print("\t"*tabs +  "--" + fileObj)
- 18                         printDirs(path+"/"+fileObj,tabs)
- 19         tabs = tabs -1
- 20 printDirs(path,tabs)
- '''
